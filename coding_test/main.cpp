c4fe6bb18fd3d29d0bb8096c07d1a76d0f95c93f
Sumin Eo <sumin6206@naver.com>
2020-02-01 16:17:24 +0900

    Initial Commit

---
+++ coding_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coding_test
+//
+//  Created by 어수민 on 2020/02/01.
+//  Copyright © 2020 어수민. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
